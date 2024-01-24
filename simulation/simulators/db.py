8a1e405729b79188e290a265c77104d07ddea329
Jelena Mikovic <jelena.mikovic189@gmail.com>
2024-01-24 01:06:06 +0100

    Added buzzer activation for alarm

--- simulation/simulators/db.py
+++ simulation/simulators/db.py
@@ -2,17 +2,34 @@ from queue import Empty
 from locks import lock
 import time
 import random
+import paho.mqtt.client as mqtt
+from broker_settings import HOSTNAME, PORT
+
+mqtt_client = mqtt.Client()
+mqtt_client.connect(HOSTNAME, 1883, 60)
+mqtt_client.loop_start()
+#mqtt_client.subscribe("topic/alarm")
+buzzing = False
+
+def on_message(callback, publish_event, settings, message):
+    global buzzing
+    action = message.payload.decode("utf-8")
+    if action == "on":
+        buzzing = True
+        callback(True, publish_event, settings)
+    elif action == "off":
+        buzzing = False
+        callback(False, publish_event, settings)
 
 def run_db_simulator(queue, pitch, duration, callback, stop_event, publish_event, settings):
+    mqtt_client.on_message = lambda client, userdata, message: on_message(callback, publish_event, settings, message)
     while not stop_event.is_set():
         try:
-            action = queue.get(timeout=1)
-            if action:
-                callback(True, publish_event, settings)
+            global buzzing
+            if buzzing:
                 period = 1.0 / pitch
                 delay = period / 2
                 cycles = int(duration * pitch) 
-                
                 with lock:
                     for _ in range(cycles):
                         start = time.time()
@@ -29,7 +46,6 @@ def run_db_simulator(queue, pitch, duration, callback, stop_event, publish_event
                         time.sleep(delay)
                         if stop_event.is_set():
                             break
-                callback(False, publish_event, settings)
         except Empty:
             pass
 
